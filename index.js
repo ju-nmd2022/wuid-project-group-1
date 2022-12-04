618d0becc7dd319a4f92d59c1c827a3cb1100bb7
Linus Isaksson <linusisaksson97@gmail.com>
2022-12-04 14:14:06 +0100

    minor change

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const searchBar = document.getElementById("searchBar");
 searchBar.addEventListener("keyup", (e) => {
   const searchString = e.target.value;
 });
+
+
+
+
+// java for changing elephant images on individual item page
+
