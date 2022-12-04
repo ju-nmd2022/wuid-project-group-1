afd84191bd221027a65096a9e5d591de850b5bda
Linus Isaksson <linusisaksson97@gmail.com>
2022-12-04 17:19:06 +0100

    Images change onclick on elephantpage

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const searchBar = document.getElementById("searchBar");
+// const searchBar = document.getElementById("searchBar");
 
-searchBar.addEventListener("keyup", (e) => {
-  const searchString = e.target.value;
-});
+// searchBar.addEventListener("keyup", (e) => {
+//   const searchString = e.target.value;
+// });
 
 
 
 
 // java for changing elephant images on individual item page
 
+function changeImage(fileName){
+  let img = document.querySelector("#bigelephantimg");
+  img.setAttribute("src", fileName);
+}
