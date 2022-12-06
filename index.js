1478d8a2f157b8045d43749f0086af0a6f473649
Linus Isaksson <linusisaksson97@gmail.com>
2022-12-06 13:09:31 +0100

    automatic image switcher

--- index.js
+++ index.js
@@ -13,3 +13,29 @@ function changeImage(fileName){
   let img = document.querySelector("#bigelephantimg");
   img.setAttribute("src", fileName);
 }
+
+// java for automatically chaning images every 3 seconds
+
+// https://www.youtube.com/watch?v=4YQ4svkETS0
+// used this guide with help
+
+var i = 0;
+var images = [];
+var time = 3000;
+
+images[0] = 'images/grogu.webp';
+images[1] = 'images/toysale.jpeg';
+
+function changeImg (){
+  document.slide.src = images[i];
+
+  if(i < images.length - 1){
+    i++;
+  } else {
+    i = 0;
+
+  }
+  setTimeout("changeImg()", time);
+}
+
+window.onload = changeImg;
